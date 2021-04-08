a81a4648ed06cbb4743d5660b4601c98ad0fe25a
DuyTran <duy88tran@gmail.com>
2021-04-08 11:09:11 +0700

    deploy

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import Carousel from "./components/Carousel";
+import ProductList from "./components/ProductList";
+import Footer from "./components/Footer";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Carousel />
+      <ProductList />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
